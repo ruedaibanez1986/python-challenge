b25aa60adc04d614e67c9ef0ffd905daf32d1b3d
Jorge Rueda <rueda.ibanez.jr@gmail.com>
2020-02-16 12:15:59 -0600

    first part of code reading and printing total votes

--- PyElections/Emain.py
+++ PyElections/Emain.py
@@ -1,14 +1,26 @@
 import os
 import csv
 
-with open('houston_election_data.csv', 'r') as csv_file:
+with open('houston_election_data.csv',newline="",encoding="utf-8") as csv_file:
         csv_reader = csv.reader(csv_file)
+        csv_header = next(csv_file)
+        
+        row_count = 0
+        candidates_name = []
 
         for row in csv_reader:
-            print(row)
 
-       
+            row_count = row_count + 1
+
+            candidates = row[0]
+            if candidates not in candidates_name : 
+                candidates_name.append(candidates)
 
 
+# Setting up the print of the information calculated
+print("---------------------------------------------")
+print("Total Votes")
+print(row_count)
+print("---------------------------------------------")
 
-
+print(candidates_name)
