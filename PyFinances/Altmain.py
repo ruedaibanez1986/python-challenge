b0bb0ef2d6f47c35c13090971ae2643387d98d64
Jorge Rueda <rueda.ibanez.jr@gmail.com>
2020-02-22 18:06:12 -0600

    added correction for Average Variation to the file MainFinance

--- PyFinances/Altmain.py
+++ PyFinances/Altmain.py
@@ -1,72 +1,63 @@
 #import modules
 import os
 import csv
-import statistics
+from statistics import mean
 
-#creating trackers
 
+
+#opening the csv file 
 with open('budget_data.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
+    #excluding headers of the csv file
     csv_header = next(csv_file)
 
+    
 #Creating counters
     total_months = 0
-    pnl_total = 0
+    pnl_total = []
     past_pnl = 0
     g_increase = 0
     l_decrease = 99999999
     profit_change = []
+    pnl_var = []
 
-       
+    
     for row in csv_reader:
+        #count total months in data set
         total_months = total_months + 1
-        pnl_total += int(row[0])
-        monthly_profit_var = int(row[0]) - past_pnl
+        #calculate total p&l of the data set 
+        pnl_total.append(row[0])
         past_pnl = int(row[0])
-        profit_change.append(monthly_profit_var)
-        var_profit_change = round(sum(profit_change)/total_months)
 
-        if (monthly_profit_var > g_increase):
-            greatest_increase_month = row[1]
-            g_increase = monthly_profit_var
-        if (monthly_profit_var < l_decrease):
-            lowest_decrease_month = row[1]
-            l_decrease = monthly_profit_var
-Results = (
-f" Financial Analysis \n"
-f"-------------------------\n"
-f"Total Months: {total_months} \n"
-f"Total: ${pnl_total} \n"
-f"Average Change: ${var_profit_change} \n"
-f"Greatest Increase in Profit: {greatest_increase_month} ${g_increase} \n"
-f"Greates Decrease in Profit: {lowest_decrease_month} ${l_decrease} \n"
-)
+    for i in range (len(pnl_total)):
 
+        if i != 0:
+            pnl_var.append(int(pnl_total[i])-int(pnl_total[i-1]))
 
-publish_data = os.path.join('py_finance_sanalisis/finance_data_results.txt')
+             
+     #Testing if I am adding the var to the determing list   
+    #print(pnl_var)
 
-with open(publish_data, 'w') as txtfile:
-    txtwriter = txtfile.write(Results)
+    max_limit = max(pnl_var)
+    min_limit = min(pnl_var)
+    avg_change = mean(pnl_var)
+    print("-------------------------------")
+    print ("Financial Analysis")
+    print("-------------------------------")
+    print("Total Months")
+    print(total_months)
+    print("-------------------------------")
+    print("Average Change")
+    print("-------------------------------")
+    print (avg_change)
+    print("Greatest Increase in Profit")
+    print(max_limit)
+    print("Greatest Dcrease in Profit")
+    print(min_limit)
+    
     
 
 
 
-        
 
-
-
- #Test Results out of the format     
-print(Results)
-print("----------------------")
-print("----------------------")
-print("----------------------")
-print("Total Months")
-print(total_months)
-print("----------------------")
-print("Total P&L")
-print(pnl_total)
-print("-----------------------")
-print(var_profit_change)
-
-
-
+    
